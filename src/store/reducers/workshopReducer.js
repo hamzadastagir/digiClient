fb1063f85809a6a128a9f5092aff1746417af744
sajid <asajid451@gmail.com>
2018-09-06 03:44:13 +0500

    action methods to get list of workshops from data base

--- src/store/reducers/workshopReducer.js
+++ src/store/reducers/workshopReducer.js
@@ -4,10 +4,14 @@ import {
   CREATE_WORKSHOP_REQUEST,
   CREATE_WORKSHOP_SUCCESS,
   CREATE_WORKSHOP_FAILED,
+  GET_WORKSHOP_REQUEST,
+  GET_WORKSHOP_SUCCESS,
+  GET_WORKSHOP_FAILED,
 } from '../constants';
 
 // The initial state of the App
 export const initialState = fromJS({
+  data: [],
   workshop: {},
   success: false,
   loading: false
@@ -20,6 +24,12 @@ function workshopReducer(state = initialState, action) {
     case CREATE_WORKSHOP_SUCCESS:
       return state.set('workshop', action.payload).set('success', true).set('loading', false);
     case CREATE_WORKSHOP_FAILED:
+      return state.set(initialState);
+    case GET_WORKSHOP_REQUEST:
+      return state.set('data', []).set('success', false).set('loading', true);
+    case GET_WORKSHOP_SUCCESS:
+      return state.set('data', action.payload).set('success', true).set('loading', false);
+    case GET_WORKSHOP_FAILED:
       return state.set(initialState);
     default:
       return state;
