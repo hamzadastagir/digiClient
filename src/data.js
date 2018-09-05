708fd50192987f5585984682c624bcf09b96483c
sajid <asajid451@gmail.com>
2018-09-06 03:46:05 +0500

    remove mock data from UI

--- src/data.js
+++ src/data.js
@@ -36,20 +36,6 @@ const data = {
       {id: 4, title: 'Social Marketing', text: 'Use of social media for digitization'}
     ],
     
-    workshops: [
-      {name: 'Jan', uv: 3700},
-      {name: 'Feb', uv: 3000},
-      {name: 'Mar', uv: 2000},
-      {name: 'Apr', uv: 2780},
-      {name: 'May', uv: 2000},
-      {name: 'Jun', uv: 1800},
-      {name: 'Jul', uv: 2600},
-      {name: 'Aug', uv: 2900},
-      {name: 'Sep', uv: 3500},
-      {name: 'Oct', uv: 3000},
-      {name: 'Nov', uv: 2400},
-      {name: 'Dec', uv: 2780}
-    ],
     genres: [
       {name: 'Jan', uv: 3700},
       {name: 'Feb', uv: 3000},
